e4da0399a508fd83974f069598f574915f64d7c1
haneyakhan2249 <haneyakhan2249@gmail.com>
2017-12-04 11:24:02 -0600

    Add Group class with relation to Contact

---
+++ App/Contacts/app/src/main/java/me/saipathuri/contacts/Group.java
@@ -0,0 +1,47 @@
+package me.saipathuri.contacts;
+
+import java.util.ArrayList;
+import io.objectbox.annotation.Entity;
+import io.objectbox.annotation.Id;
+import io.objectbox.relation.ToMany;
+import io.objectbox.relation.ToOne;
+
+/**
+ * Created by haney on 12/2/2017.
+ */
+
+@Entity
+public class Group {
+
+    @Id
+    long id;
+
+    String groupName;
+
+    ToMany<Contact> contacts;
+
+    public Group(long id, String groupName) {
+        this.id = id;
+        this.groupName = groupName;
+    }
+
+    public Group(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+}
