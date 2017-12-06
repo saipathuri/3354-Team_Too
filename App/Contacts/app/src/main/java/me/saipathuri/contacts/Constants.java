95210ef5eb748adf4cfd6004064ea068d96d3ce0
haneyakhan2249 <haneyakhan2249@gmail.com>
2017-12-05 18:58:39 -0600

    Add relation to contact class

--- App/Contacts/app/src/main/java/me/saipathuri/contacts/Constants.java
+++ App/Contacts/app/src/main/java/me/saipathuri/contacts/Constants.java
@@ -8,6 +8,8 @@ public class Constants {
 
     public static final String CONTACT_ID_EXTRA_KEY = "contact_id";
     public static final String REQUIRED_INFO_MISSING_MSG = "Please enter at least the first name and at least one phone number";
+    public static final String REQUIRED_GROUP_INFO_MISSING_MSG = "Please enter at least the group name and a group member.";
     public static final String CONTACT_EDIT_SUCCESS = "Contact successfully saved.";
+    public static final String GROUP_EDIT_SUCCESS = "Group successfully saved.";
     public static final String SEARCH_UNSUCCESSFUL = "Search was unsuccessful.";
 }
